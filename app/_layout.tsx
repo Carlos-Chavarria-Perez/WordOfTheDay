87b8870d97104d3424c872207f7014645fea7b3e
Carlos Chavarria <chato12_7@hotmail.com>
2026-02-14 13:04:46 -0600

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,10 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
-import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <>
       <Stack screenOptions={{ headerShown: false }} />
-      <Toast/>
-      </>
     </AuthProvider>
   );
 }
